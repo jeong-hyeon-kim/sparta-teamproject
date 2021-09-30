53b61bf68edf989874221feb2764927a427aef2f
jeonghyeon <skjh0807s@snu.ac.kr>
2021-09-30 22:59:38 +0900

    반응형 정돈

--- static/js/slide.js
+++ static/js/slide.js
@@ -1,3 +1,7 @@
+// 화면 너비//
+var windowWidth = $( window ).width();
+
+
 let slideNext = document.querySelector(".slide-next");
 let slidePrev = document.querySelector(".slide-prev");
 
@@ -6,34 +10,30 @@ let card = document.querySelectorAll(".qna-showcard");
 let currentIndex = 0;
 let cardCount = card.length;
 let cardWidth = 350;
-let cardMargin = 50;
+let cardMargin = 20;
 
 qnaBox.style.width = (cardWidth + cardMargin) * cardCount - cardMargin + 'px';
 
-
-function moveSlide(num) {
-    qnaBox.style.left = (-num * 800 + 'px');
+function moveSlide1(num) {
+    qnaBox.style.left = (-num * 740 + 'px');
     currentIndex = num;
 };
 
 
 slideNext.addEventListener('click', function() {
     if (currentIndex < 2){
-        moveSlide(currentIndex + 1);
+        moveSlide1(currentIndex + 1);
         console.log(currentIndex);
     } else {
-        moveSlide(0);
+        moveSlide1(0);
     }
 })
 
 slidePrev.addEventListener('click', function() {
     if (currentIndex > 0){
-        moveSlide(currentIndex - 1);
+        moveSlide1(currentIndex - 1);
         console.log(currentIndex);
     } else {
-        moveSlide(2);
+        moveSlide1(2);
     }
 })
-
-
- 
