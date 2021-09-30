bfee8286bd6d7facf7fb96e7f1e3686e277ad5ac
jeonghyeon <skjh0807s@snu.ac.kr>
2021-10-01 01:32:38 +0900

    메인페이지 슬라이드 수정 중

--- static/js/slide.js
+++ static/js/slide.js
@@ -6,34 +6,61 @@ let slideNext = document.querySelector(".slide-next");
 let slidePrev = document.querySelector(".slide-prev");
 
 let qnaBox = document.querySelector(".qna-showlist");
-let card = document.querySelectorAll(".qna-showcard");
+let card = document.querySelectorAll(".qna-showlist li");
 let currentIndex = 0;
 let cardCount = card.length;
-let cardWidth = 350;
+let cardWidth = 330;
 let cardMargin = 20;
 
-qnaBox.style.width = (cardWidth + cardMargin) * cardCount - cardMargin + 'px';
+makeClone();
 
-function moveSlide1(num) {
-    qnaBox.style.left = (-num * 740 + 'px');
-    currentIndex = num;
-};
-
-
-slideNext.addEventListener('click', function() {
-    if (currentIndex < 2){
-        moveSlide1(currentIndex + 1);
-        console.log(currentIndex);
-    } else {
-        moveSlide1(0);
+function makeClone() {
+    for (var i = 0; i < cardCount; i++) {
+        var cloneCard = card[i].cloneNode(true);
+        cloneCard.classList.add('clone');
+        qnaBox.appendChild(cloneCard);
     }
-})
-
-slidePrev.addEventListener('click', function() {
-    if (currentIndex > 0){
-        moveSlide1(currentIndex - 1);
-        console.log(currentIndex);
-    } else {
-        moveSlide1(2);
+    for (var i =cardCount -1; i>=0; i--) {
+        var cloneCard = card[i].cloneNode(true);
+        cloneCard.classList.add('clone');
+        qnaBox.prepend(cloneCard);
     }
-})
+    updateWidth();
+}
+
+function updateWidth() {
+    var currentCards = document.querySelectorAll(".qna-showlist li");
+    var newCardCount = currentCards.length;
+
+    var newWidth = (cardWidth + cardMargin) * newCardCount - cardMargin + 'px';
+    qnaBox.style.width = newWidth;
+}
+
+
+
+//이전 버전
+// qnaBox.style.width = (cardWidth + cardMargin) * cardCount - cardMargin + 'px';
+
+// function moveSlide1(num) {
+//     qnaBox.style.left = (-num * 740 + 'px');
+//     currentIndex = num;
+// };
+
+
+// slideNext.addEventListener('click', function() {
+//     if (currentIndex < 2){
+//         moveSlide1(currentIndex + 1);
+//         console.log(currentIndex);
+//     } else {
+//         moveSlide1(0);
+//     }
+// })
+
+// slidePrev.addEventListener('click', function() {
+//     if (currentIndex > 0){
+//         moveSlide1(currentIndex - 1);
+//         console.log(currentIndex);
+//     } else {
+//         moveSlide1(2);
+//     }
+// })
---
+++ static/js/slide2.js
@@ -0,0 +1,9 @@
+// const slidePrev = document.getElementByClassName('.slide-prev');
+// const slideNext = document.getElementByClassName('.slide-Next');
+
+// let cardList = 
+
+// function transformPrev(event) {}
+
+
+// slidePrev.addEventListener('click', transformPrev);
